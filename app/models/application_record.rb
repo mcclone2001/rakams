4c17401185d4b40a416039c7c1957e5fe77ac4ce
Ivan Quiñones <mcclone2001@gmail.com>
2020-07-13 17:14:48 -0500

    Feat adding paranoid and UUID id generator to ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
+  acts_as_paranoid
+  after_initialize :generate_uuid
+
+  def generate_uuid
+      self.id ||= SecureRandom.uuid
+  end
+  
   self.abstract_class = true
 end
