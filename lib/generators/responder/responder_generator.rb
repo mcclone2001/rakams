bac92e8fce9f29c04dede941312e2a7e75b3ebf6
Ivan Quiñones <mcclone2001@gmail.com>
2020-07-17 17:21:58 -0500

    Feat generador de responders

--- lib/generators/responder/responder_generator.rb
+++ lib/generators/responder/responder_generator.rb
@@ -1,10 +1,9 @@
 class ResponderGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
-  argument :topic, type: :string, default: [], banner: "topic"
-  argument :object, type: :string, default: [], banner: "object"
+  argument :topic, type: :string, banner: "topic", required: true
 
   def copy_initializer_file
-    template "responder.erb", "app/responders/#{file_name}_responder.rb"
+    template "responder.erb", "app/responders/#{file_name}.rb"
   end
 end
