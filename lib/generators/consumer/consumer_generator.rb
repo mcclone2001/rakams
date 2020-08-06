557247a90f3311443250bacce187ed8a34c681e6
Ivan Quiñones <mcclone2001@gmail.com>
2020-08-06 18:11:14 -0500

    Feat consumer generator registra consumer en karafka.rb

--- lib/generators/consumer/consumer_generator.rb
+++ lib/generators/consumer/consumer_generator.rb
@@ -7,6 +7,27 @@ class ConsumerGenerator < Rails::Generators::NamedBase
   argument :topic, type: :string, banner: 'topic', required: true
 
   def copy_initializer_file
+    template = fetch_template
+    contenido = fetch_karafka_app_config_file
+    unless contenido.match?(/#{template}/)
+      contenido_modificado = contenido.sub(/consumer_groups.draw do/mi, template)
+    end
+    File.open('karafka.rb', 'wb') { |file| file.write(contenido_modificado) }
     template 'consumer.erb', "app/consumers/#{file_name}.rb"
+  end
+
+  def fetch_template
+    [
+      'consumer_groups.draw do',
+      "    topic :#{topic} do",
+      "      consumer #{class_name}",
+      '    end'
+    ].join("\n") + "\n"
+  end
+
+  def fetch_karafka_app_config_file
+    contenido = ''
+    File.open('karafka.rb', 'rb') { |file| contenido = file.read }
+    contenido
   end
 end
